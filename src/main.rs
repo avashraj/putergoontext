e242bca25386635c2afb84213f9de131ba9712f2
Avash Adhikari <avashraj328@gmail.com>
2025-06-29 23:22:02 -0700

    chore: change gui lib to iced

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,37 @@
-use eframe::egui;
-
-fn main() {
-    let native_options = eframe::NativeOptions::default();
-
-    let _ = eframe::run_native(
-        "think",
-        native_options,
-        Box::new(|cc| Ok(Box::new(MyEguiApp::new(cc)))),
-    );
-}
+use iced::widget::{Column, button, column, text};
 
 #[derive(Default)]
-struct MyEguiApp {}
+struct Counter {
+    value: i64,
+}
 
-impl MyEguiApp {
-    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
-        // Restore app state using cc.storage (requires the "persistence" feature).
-        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
-        // for e.g. egui::PaintCallback.
-        Self::default()
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    Increment,
+    Decrement,
+}
+
+impl Counter {
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::Increment => {
+                self.value += 1;
+            }
+            Message::Decrement => {
+                self.value -= 1;
+            }
+        }
+    }
+
+    fn view(&self) -> Column<Message> {
+        column![
+            button("+").on_press(Message::Increment),
+            text(self.value),
+            button("-").on_press(Message::Decrement),
+        ]
     }
 }
 
-impl eframe::App for MyEguiApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("think");
-        });
-    }
+pub fn main() {
+    let _ = iced::run("COUNTER!", Counter::update, Counter::view);
 }
